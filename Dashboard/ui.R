cca2f4a6598c874097db83408b261a8a36b98732
Lachlan-Cassin <cassinlachlan@gmail.com>
2025-08-30 23:04:38 +1000

    Update ui.R

--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -4,7 +4,11 @@ ui <- htmlTemplate(
   "template.html",
   title = "Data Centre Location Dashboard",
   pageHeading = "Data Centre Location Dashboard",
-  scores = DT::DTOutput("scoreTable"),        # list/search area
-  map    = leafletOutput("map", height = 500),
-  risk   = plotOutput("riskPlot", height = 300)
+
+  
+  #Data Cells / Graphic Displays
+  
+  scores = DT::DTOutput("scoreTable"),           # sample list/search area - rank scores, select individual locations, etc.
+  map    = leafletOutput("map", height = 500),   # sample map - replace with GK's map
+  risk   = plotOutput("riskPlot", height = 300)  # sample chart - more likely for specific information on selected location
 )
